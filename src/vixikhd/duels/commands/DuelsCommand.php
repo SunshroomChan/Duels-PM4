080d97a774a95d7096a505323e03de5f867ff23e
SunshroomChan <89954408+SunshroomChan@users.noreply.github.com>
2022-02-20 09:59:52 +0700

    Refix code from NovaStark1234

--- src/vixikhd/duels/commands/DuelsCommand.php
+++ src/vixikhd/duels/commands/DuelsCommand.php
@@ -46,9 +46,9 @@ class DuelsCommand extends Command implements PluginOwned
      * @return mixed|void
      */
 
-    public function __construct(Duels $plugin)
- 
-        $this->plugin = $plugin
+    public function __construct(Duels $plugin){
+        $this->plugin = $plugin;
+
         parent::__construct("duels", "Duels Command", "§cUse /duels help or /dl help to see list of commands!", ["dl"]);
         $this->setPermission("duels.cmd");
     }
